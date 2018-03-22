46ea9f1644305990466f154127c14e48bb11a897
student <student@CSIT0059873.cse.local>
2018-03-22 19:03:55 -0400

    testing git in eclipse

--- ShipProject/src/ShipProjectTest.java
+++ ShipProject/src/ShipProjectTest.java
@@ -5,7 +5,7 @@ public class ShipProjectTest {
 	Ship[] shipArray = new Ship[3];
 	shipArray[0] =  new Ship("SS Ship", 1995);
 	shipArray[1] =  new CargoShip("SS Cargo Ship", 1985, 1500);
-	shipArray[2] = new CruiseShip("SS Cruise Ship", 2002, 300);
+	 shipArray[2] = new CruiseShip("SS Cruise Ship", 2002, 300);
 	System.out.println(shipArray[0].toString());
 	System.out.println(shipArray[1].toString());
 	System.out.println(shipArray[2].toString());
