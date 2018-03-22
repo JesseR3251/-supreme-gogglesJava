07af85581b7a5c1cc480771eb5e8dc65998cb564
student <student@CSIT0059873.cse.local>
2018-03-22 19:11:08 -0400

    Another eclipse git test

--- ShipProject/src/ShipProjectTest.java
+++ ShipProject/src/ShipProjectTest.java
@@ -7,7 +7,7 @@ public class ShipProjectTest {
 	shipArray[1] =  new CargoShip("SS Cargo Ship", 1985, 1500);
 	 shipArray[2] = new CruiseShip("SS Cruise Ship", 2002, 300);
 	System.out.println(shipArray[0].toString());
-	System.out.println(shipArray[1].toString());
+	  System.out.println(shipArray[1].toString());
 	System.out.println(shipArray[2].toString());
 	}
 
