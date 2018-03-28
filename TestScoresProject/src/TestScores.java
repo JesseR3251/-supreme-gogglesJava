c29f1d78658dffc4a66380da8e2da39c871e1189
Jesse Robinson <Jesse.michael.robinson@gmail.com>
2018-03-27 22:03:46 -0400

    working on hw, not done. Having issues figuring out how to do the arrays

--- TestScoresProject/src/TestScores.java
+++ TestScoresProject/src/TestScores.java
@@ -1,16 +1,18 @@
 
 public class TestScores {
-	double[] testScores = {55,68,78,80,100};
+	double[] testScores = new double[] {0,0,0};
 	
 	public double[] getTestScoreArray() {
 		return testScores;
 	}
-	
+//	If any test score in the array is negative
+//	or greater than 100, the class should throw an IllegalArgumentException. 
+
 	public void setTestScoreArray(double[] testScores) {
 		this.testScores = testScores;
 	}
 	
-	public double scoreAverage(double[] scoreArray) {
+	public double scoreAverage(double[] scoreArray) throws IllegalArgumentException {
 		double total = 0;
 		
 		for (int i=0; i < scoreArray.length;i++) {
--- TestScoresProject/src/TestScoresTest.java
+++ TestScoresProject/src/TestScoresTest.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
 
 public class TestScoresTest {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IllegalArgumentException {
+		
 		TestScores ts = new TestScores();
-		System.out.println(ts.scoreAverage(ts.getTestScoreArray()));
+		System.out.println(ts.getTestScoreArray());
+//		Scanner sc = new Scanner(System.in);
+//		System.out.print("How many test scores are you entering in? ");
+//		
+
+//		System.out.println(ts.scoreAverage(ts.getTestScoreArray()));
 
 	}
 
