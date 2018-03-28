4f7ed42780eef9248425f2d856870cb5ade6ba76
Jesse Robinson <jrobinson@bhg-inc.com>
2018-03-28 13:14:49 -0400

    Created array using userInput, need to correct the error exception

--- TestScoresProject/src/TestScores.java
+++ TestScoresProject/src/TestScores.java
@@ -1,19 +1,20 @@
 
 public class TestScores {
-	double[] testScores = new double[] {0,0,0};
 	
-	public double[] getTestScoreArray() {
+	public String getTestScoreArray(double[] scoreArray) {
+		String testScores = "";
+		for (int i=0; i < scoreArray.length;i++) {
+			testScores = testScores + " / " + scoreArray[i];
+		}
 		return testScores;
 	}
 //	If any test score in the array is negative
 //	or greater than 100, the class should throw an IllegalArgumentException. 
 
-	public void setTestScoreArray(double[] testScores) {
-		this.testScores = testScores;
-	}
+
 	
-	public double scoreAverage(double[] scoreArray) throws IllegalArgumentException {
-		double total = 0;
+	public double scoreAverage(double[] scoreArray) {
+		double total = 0; 
 		
 		for (int i=0; i < scoreArray.length;i++) {
 			total = total + scoreArray[i];
--- TestScoresProject/src/TestScoresTest.java
+++ TestScoresProject/src/TestScoresTest.java
@@ -3,11 +3,25 @@ import java.util.Scanner;
 public class TestScoresTest {
 
 	public static void main(String[] args) throws IllegalArgumentException {
+		System.out.print("How many test scores are you entering in? ");
+		Scanner sc = new Scanner(System.in);
+		int userInput = sc.nextInt();
+		
+		
+		double[] scores = new double[userInput];
+		
+		for (int i=0;i < scores.length; i++) {
+			System.out.print("Enter test score: ");
+			scores[i] = sc.nextInt();
+			if (userInput < 0 || userInput > 100) {
+				throw new IllegalArgumentException("wow ");
+			}
+		}
 		
 		TestScores ts = new TestScores();
-		System.out.println(ts.getTestScoreArray());
-//		Scanner sc = new Scanner(System.in);
-//		System.out.print("How many test scores are you entering in? ");
+		System.out.println(ts.getTestScoreArray(scores));
+
+
 //		
 
 //		System.out.println(ts.scoreAverage(ts.getTestScoreArray()));
